9736074400a886770801fa9189702e7edf8fa2e7
shao <shaoqing@zdns.cn>
2024-05-09 11:09:03 +0800

    log

--- dump.go
+++ dump.go
@@ -21,19 +21,26 @@ type Dump struct {
 	Next plugin.Handler
 }
 
-func init() { plugin.Register("dump", setup) }
+func init() {
+	fmt.Println("init")
+	plugin.Register("dump", setup)
+}
 
 func setup(c *caddy.Controller) error {
+	fmt.Println("setup(): start")
 	for c.Next() {
+		fmt.Println("setup(): c.Next()")
 		if c.NextArg() {
+			fmt.Println("setup(): c.NextArg()")
 			return plugin.Error("dump", c.ArgErr())
 		}
 	}
 
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		fmt.Println("setup(): AddPlugin()")
 		return Dump{Next: next}
 	})
-
+	fmt.Println("setup(): ok")
 	return nil
 }
 
@@ -50,16 +57,21 @@ func (d Dump) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (i
 	state := request.Request{W: w, Req: r}
 	rep := replacer.New()
 	trw := dnstest.NewRecorder(w)
-	// Wrap.
-	pw := NewResponsePrinter(w)
 
 	fmt.Fprintln(output, rep.Replace(ctx, state, trw, format))
 	fmt.Println("ServeDNS(): replace:\r\n", rep.Replace(ctx, state, trw, format))
-	return plugin.NextOrFailure(d.Name(), d.Next, ctx, pw, r)
+	return plugin.NextOrFailure(d.Name(), d.Next, ctx, w, r)
 }
 
 // Name implements the Handler interface.
-func (d Dump) Name() string { return "dump" }
+func (d Dump) Name() string {
+	fmt.Println("Name()")
+	return "dump"
+}
+func (d Dump) Ready() bool {
+	fmt.Println("Ready()")
+	return true
+}
 
 // ResponsePrinter wrap a dns.ResponseWriter and will write example to standard output when WriteMsg is called.
 type ResponsePrinter struct {
